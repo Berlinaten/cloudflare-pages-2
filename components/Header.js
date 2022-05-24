a31a0487b8b77cae7da426a1a479ee50e48b8812
Berlinaten <80010558+Berlinaten@users.noreply.github.com>
2022-05-24 09:10:57 +0200

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
         <header>
             <div className='container'>
                 <Link href='/' passHref>
-                    <h2>Dev Blog</h2>
+                    <h4>Gesundheit und Medizin</h4>
                 </Link>
             </div>
         </header>

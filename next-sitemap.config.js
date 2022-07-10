81a4c0668ebcea583d4e5a06bc0eb6c85b1e2001
Berlinaten <80010558+Berlinaten@users.noreply.github.com>
2022-07-10 21:04:05 +0200

    Create next-sitemap.config.js

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: 'https://cloudflare-pages-2-5zi.pages.dev/',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+}
+
+export default config
